b1f0d595b7b5058ae2d86deecbfee3c059d78624
Sergio Scotta <sergeen@gmail.com>
2024-02-16 01:38:25 -0300

    Germination: some experimentation with p5.js

---
+++ p5/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function concentricCircles() {
+  let size = width < height ? width : height;
+  let circles = 10;
+  let step = size / circles;
+  for (let i = 0; i < circles; i++) {
+    this.circle(
+      // a randowm width never greater than 70% of the canvas
+      random(width * 0.7),
+      random(height * 0.7),
+      size - i * step
+    )
+  }
+}
+
+function draw() {
+  background(0);
+  noStroke();
+  const c = color(255, 255, 255, 10);
+  fill(c);
+  concentricCircles();
+  concentricCircles();
+  concentricCircles();
+  concentricCircles();
+  frameRate(5);
+  filter(BLUR, 20);
+  describe('This is realtime accesible?.');
+}
